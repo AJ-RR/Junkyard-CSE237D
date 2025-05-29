8f17e2d29ae9b9c6a06b8daf0feb59d5efa4f059
ayangaur26 <85892331+ayangaur26@users.noreply.github.com>
2025-05-29 11:59:08 -0700

    Test for metric evaluation

---
+++ container-images/job-server/metrics.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"time"
+)
+
+const stateFile = "/app/latency_state.json"
+
+type latencyState struct {
+	MinStart      time.Time `json:"min_start"`
+	MaxEnd        time.Time `json:"max_end"`
+	TotalSeconds  float64   `json:"total_seconds"`
+	Jobs          int       `json:"jobs"`
+}
+
+var state latencyState
+
+func init() {
+	// Try to load previous state (survives pod restarts if /app is persisted)
+	data, err := os.ReadFile(stateFile)
+	if err == nil {
+		_ = json.Unmarshal(data, &state)
+	}
+}
+
+func updateLatency(start, end time.Time) {
+	delta := end.Sub(start).Seconds()
+
+	// first job ever
+	if state.Jobs == 0 || start.Before(state.MinStart) {
+		state.MinStart = start
+	}
+	if end.After(state.MaxEnd) {
+		state.MaxEnd = end
+	}
+	state.TotalSeconds += delta
+	state.Jobs++
+
+	save()
+}
+
+func save() {
+	file, err := os.CreateTemp("/app", "lat_tmp_*")
+	if err != nil {
+		log.Printf("latency save tmp err: %v", err)
+		return
+	}
+	enc := json.NewEncoder(file)
+	if err := enc.Encode(state); err != nil {
+		log.Printf("latency encode err: %v", err)
+	}
+	file.Close()
+	_ = os.Rename(file.Name(), stateFile) // atomic replace
+}

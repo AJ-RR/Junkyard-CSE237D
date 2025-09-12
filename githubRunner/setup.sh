590c37fe6ec60e498793774ee15aa8c78850e283
Arunan Thiviyanathan <arunankogulan@gmail.com>
2025-09-11 22:51:30 -0500

    Adjusted Setup to use Github App

--- githubRunner/setup.sh
+++ githubRunner/setup.sh
@@ -3,10 +3,17 @@ echo "🔧 GitHub Actions Runner Setup Wizard"
 printf "Enter your GitHub org URL (e.g. https://github.com/my-org/my-repo): "
 read GITHUB_CONFIG_URL
 
-printf "Create a Github Personal Access Token (classic)\n"
-printf "Note: Ensure that your token as scope admin:org \n"
-printf "Enter your GitHub Personal Access Token: "
-read GITHUB_PAT
+printf "Create a new Application for your Github organization\n"
+printf "Enter the newly made Application ID:\n"
+read GITHUB_APP_ID
+
+printf "Install the newly made application to your class organization\n"
+printf "Enter the installation ID (seen in the URL after installing): \n"
+read GITHUB_INSTALLATION_ID
+
+printf "Download a Private Key from the App Settings Page\n"
+printf "Enter the path to the private key: "
+read GITHUB_PRIVATE_KEY_PATH
 
 helm install arc \
     --namespace "greengrader-systems" \
@@ -17,5 +24,11 @@ helm install "greengrader" \
     --namespace "greengrader-runners" \
     --create-namespace \
     --set githubConfigUrl="$GITHUB_CONFIG_URL" \
-    --set githubConfigSecret.github_token="$GITHUB_PAT" \
+    --set githubConfigSecret="greengrader-auth" \
     oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
+
+kubectl create secret generic greengrader-auth \
+   --namespace=greengrader-runners \
+   --from-literal=github_app_id=$GITHUB_APP_ID \
+   --from-literal=github_app_installation_id=$GITHUB_INSTALLATION_ID \
+   --from-file=github_app_private_key=$GITHUB_PRIVATE_KEY_PATH
